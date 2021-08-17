d5c50dd6a2854d9b339334374422f99d7e1d9ae7
liguo <liguo@ireadercity.com>
2021-08-17 10:28:17 +0800

    --

--- ad/src/main/java/cn/yq/ad/self/SplashForSelf.java
+++ ad/src/main/java/cn/yq/ad/self/SplashForSelf.java
@@ -1,6 +1,9 @@
 package cn.yq.ad.self;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -35,25 +38,25 @@ import cn.yq.ad.proxy.model.AdRespItem;
 import cn.yq.ad.util.AdLogUtils;
 
 public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
-    private ViewGroup gdtContainer;
+    private final ViewGroup gdtContainer;
     private final String appId;
     private final String posId;
-    private Activity act;
+    private final WeakReference<Activity> wrAct;
     private static final String TAG = SplashForSelf.class.getSimpleName();
 
+    private final TextView layoutSplashForSkipView;
+    private final ImageView layoutSplashForSelfIv;
+    private final View rootView;
     public SplashForSelf(Activity act, ViewGroup gdtContainer, String appId, String posId) {
         this.gdtContainer = gdtContainer;
         this.appId = replaceTrim_R_N(appId);
         this.posId = replaceTrim_R_N(posId);
-        this.act = act;
+        this.wrAct = new WeakReference<>(act);
         this.rootView = LayoutInflater.from(act).inflate(R.layout.layout_splash_for_self,null);
         this.layoutSplashForSelfIv = rootView.findViewById(R.id.layout_splash_for_self_iv);
         this.layoutSplashForSkipView = rootView.findViewById(R.id.layout_splash_for_skip_view);
     }
 
-    private TextView layoutSplashForSkipView;
-    private ImageView layoutSplashForSelfIv;
-    private View rootView;
     @Override
     public void onClick(View v) {
         if(v == layoutSplashForSkipView){
@@ -76,11 +79,15 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
         AdLogUtils.i(TAG, "load()");
         layoutSplashForSelfIv.setOnClickListener(this);
         layoutSplashForSkipView.setOnClickListener(this);
-        Glide.with(act).load(param.getImageUrl())
-                .skipMemoryCache(true)
-//                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .listener(new TempRequestListener(this))
-                .preload();
+        Activity act = wrAct.get();
+        if(act != null){
+            Context ctx = act.getApplicationContext();
+            if(ctx != null){
+                Glide.with(ctx).load(param.getImageUrl())
+                        .listener(new TempRequestListener(this))
+                        .preload();
+            }
+        }
     }
 
     @Override
@@ -132,7 +139,11 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
     @Override
     public void destroy() {
         super.destroy();
-        layoutSplashForSelfIv.setImageBitmap(null);
+        clearDrawable();
+    }
+
+    private void clearDrawable(){
+        layoutSplashForSelfIv.setImageDrawable(null);
     }
 
     private static final String SKIP_TEXT = "%d s 跳过";
@@ -151,7 +162,7 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
         private final WeakReference<SplashForSelf> wrAdImpl;
 
         /** 是否已调用了Dismissed()方法 */
-        private boolean isCalledDismissedMethod = false;
+        private boolean mDismissFlag = false;
         public AdShowTimer(long millisInFuture, long countDownInterval,SplashForSelf splashForSelf) {
             super(millisInFuture, countDownInterval);
             this.wrAdImpl = new WeakReference<>(splashForSelf);
@@ -166,8 +177,9 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
             }
             sfs.updateSkipViewText(num);
             if(sfs.isSkiped.get()){
-                if(!isCalledDismissedMethod){
-                    isCalledDismissedMethod = true;
+                sfs.clearDrawable();
+                if(!isDismissed()){
+                    setDismissed(true);
                     sfs.defaultCallback.onAdDismissed(DismissModel.newInstance(sfs.posId, sfs.getAdvType()).setAdRespItem(sfs.getAdParamItem()));
                 }
                 cancel();
@@ -180,11 +192,20 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
             if(sfs == null){
                 return;
             }
-            if(!isCalledDismissedMethod){
-                isCalledDismissedMethod = true;
+            if(!isDismissed()){
+                setDismissed(true);
+                sfs.clearDrawable();
                 sfs.defaultCallback.onAdDismissed(DismissModel.newInstance(sfs.posId, sfs.getAdvType()).setAdRespItem(sfs.getAdParamItem()));
             }
         }
+
+        private boolean isDismissed(){
+            return mDismissFlag;
+        }
+        private void setDismissed(Boolean bool){
+            this.mDismissFlag = bool;
+        }
+
     }
 
     private static class TempRequestListener implements RequestListener<Drawable>{
@@ -210,9 +231,17 @@ public class SplashForSelf extends ADBaseImpl implements View.OnClickListener {
         public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
             Log.i(TAG,"onResourceReady()");
             SplashForSelf self = wr.get();
-            if(self != null){
-                self.layoutSplashForSelfIv.setImageDrawable(resource);
-                self.defaultCallback.onAdPresent(PresentModel.getInstance(self.posId, self.getAdvType()).setAdRespItem(self.getAdParamItem()));
+            if(self != null && resource != null){
+                if(resource instanceof BitmapDrawable){
+                    BitmapDrawable bd = (BitmapDrawable)resource;
+                    Bitmap bmp = bd.getBitmap();
+                    if(bmp == null || bmp.isRecycled()){
+
+                    }else{
+                        self.layoutSplashForSelfIv.setImageDrawable(resource);
+                        self.defaultCallback.onAdPresent(PresentModel.getInstance(self.posId, self.getAdvType()).setAdRespItem(self.getAdParamItem()));
+                    }
+                }
             }
 
             return false;
