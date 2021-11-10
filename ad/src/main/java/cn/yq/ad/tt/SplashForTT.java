022719560f0f3d5dd67b494bbf0b985fe75077dd
liguo <liguo@ireadercity.com>
2021-11-10 13:50:37 +0800

    --

--- ad/src/main/java/cn/yq/ad/tt/SplashForTT.java
+++ ad/src/main/java/cn/yq/ad/tt/SplashForTT.java
@@ -12,6 +12,7 @@ import com.bytedance.sdk.openadsdk.TTAdManager;
 import com.bytedance.sdk.openadsdk.TTAdNative;
 import com.bytedance.sdk.openadsdk.TTSplashAd;
 
+import java.lang.ref.WeakReference;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -24,8 +25,8 @@ import cn.yq.ad.impl.ClickModel;
 import cn.yq.ad.impl.DismissModel;
 import cn.yq.ad.impl.FailModel;
 import cn.yq.ad.impl.PresentModel;
+import cn.yq.ad.proxy.model.AdRespItem;
 import cn.yq.ad.tt.config.TTUtil;
-import cn.yq.ad.tt.utils.TToast;
 import cn.yq.ad.util.ADHelper;
 import cn.yq.ad.util.AdSize;
 import cn.yq.ad.util.SizeUtil;
@@ -41,7 +42,7 @@ public class SplashForTT extends ADBaseImpl {
     protected final String ids;
     private Context act;
     private final String appId;
-    private ViewGroup mSplashContainer;
+    private WeakReference<ViewGroup> mSplashContainer;
     private TTSplashAd respAd;
     SplashForTT(Activity act, String appId, String tmpAdId, ViewGroup ad_container) {
         this.act = getContextFromActivity(act);
@@ -52,7 +53,7 @@ public class SplashForTT extends ADBaseImpl {
         }
         this.adId = replaceTrim_R_N(tmpAdId);
         Log.e(TAG, "SplashForTT(),this.adId="+this.adId+",appId="+appId);
-        this.mSplashContainer = ad_container;
+        this.mSplashContainer = new WeakReference<>(ad_container);
     }
     private final AtomicInteger call_back_count = new AtomicInteger(0);
     /**
@@ -78,24 +79,33 @@ public class SplashForTT extends ADBaseImpl {
         call_back_count.set(1);
         final int AD_TIME_OUT = getRequestTimeOutFromExtra();
         Log.e(TAG, "load(),accept_width="+accept_width+",accept_height="+accept_height+",超时时间="+AD_TIME_OUT);
-        mTTAdNative.loadSplashAd(adSlot,new TempAdListener(), AD_TIME_OUT);
+        mTTAdNative.loadSplashAd(adSlot,new TempAdListener(this,adId,TAG,getAdParamItem()), AD_TIME_OUT);
     }
 
     @Override
     public Adv_Type getAdvType() {
         return Adv_Type.tt;
     }
-    private void showToast(String msg) {
-        TToast.show(act, msg);
-    }
 
-    private class TempAdListener implements TTAdNative.SplashAdListener{
+    private static class TempAdListener implements TTAdNative.SplashAdListener{
         private final String randomUUID;
         private final Map<String,String> mmp;
-        public TempAdListener() {
+        private final String adId;
+        private final String TAG;
+        private final AdRespItem adRespItem;
+        private final WeakReference<SplashForTT> wrTT;
+        public TempAdListener(SplashForTT tt,String adId, String tag, AdRespItem item) {
+            this.wrTT = new WeakReference<>(tt);
+            this.adId = adId;
+            this.TAG = tag;
+            this.adRespItem = item;
             randomUUID = UUID.randomUUID().toString();
             mmp = new LinkedHashMap<>();
             mmp.put(randomUUID,randomUUID);
+        }
+
+        private SplashForTT getTT(){
+            return wrTT.get();
         }
 
         @Override
@@ -104,7 +114,10 @@ public class SplashForTT extends ADBaseImpl {
             Log.e(TAG, "onError(),err_msg="+fm.toFullMsg());
             if(mmp.containsKey(randomUUID)) {
                 mmp.remove(randomUUID);
-                defaultCallback.onAdFailed(fm.setAdRespItem(getAdParamItem()));
+                SplashForTT tt = getTT();
+                if(tt != null) {
+                    tt.defaultCallback.onAdFailed(fm.setAdRespItem(adRespItem));
+                }
             }else{
                 Log.e(TAG,"onError(),已经回调过了onTimeout()方法");
             }
@@ -116,7 +129,10 @@ public class SplashForTT extends ADBaseImpl {
             Log.e(TAG, "onTimeout(),err_msg="+fm.toFullMsg());
             if(mmp.containsKey(randomUUID)) {
                 mmp.remove(randomUUID);
-                defaultCallback.onAdFailed(fm.setAdRespItem(getAdParamItem()));
+                SplashForTT tt = getTT();
+                if(tt != null) {
+                    tt.defaultCallback.onAdFailed(fm.setAdRespItem(adRespItem));
+                }
             }else{
                 Log.e(TAG,"onTimeout(),已经回调过了onError()方法");
             }
@@ -129,50 +145,51 @@ public class SplashForTT extends ADBaseImpl {
                 Log.e(TAG, "onSplashAdLoad(),ad is null , will return");
                 return;
             }
-            respAd = ad;
+            final SplashForTT tt = getTT();
+            if(tt == null) {
+                return;
+            }
+            tt.respAd = ad;
             //检查Activity是否已销毁
-            Activity act = ADHelper.getActivityFromView(mSplashContainer);
+            ViewGroup tmpAdContainer = tt.mSplashContainer.get();
+            Activity act = ADHelper.getActivityFromView(tmpAdContainer);
             if(act != null && (act.isFinishing() || act.isDestroyed())){
                 FailModel fm = FailModel.toStr(-2,"加载成功_但Activity已销毁~",adId,Adv_Type.tt);
                 Log.e(TAG, "onSplashAdLoad()"+fm.toFullMsg());
-                defaultCallback.onAdFailed(fm.setAdRespItem(getAdParamItem()));
+                tt.defaultCallback.onAdFailed(fm.setAdRespItem(adRespItem));
                 return;
             }
             //设置不开启开屏广告倒计时功能以及不显示跳过按钮,如果这么设置，您需要自定义倒计时逻辑
             //ad.setNotAllowSdkCountdown();
-            defaultCallback.onAdPresent(PresentModel.getInstance(adId, Adv_Type.tt).setData(ad).setAdRespItem(getAdParamItem()));
+            tt.defaultCallback.onAdPresent(PresentModel.getInstance(adId, Adv_Type.tt).setData(ad).setAdRespItem(adRespItem));
             //设置SplashView的交互监听器
             ad.setSplashInteractionListener(new TTSplashAd.AdInteractionListener() {
                 @Override
                 public void onAdClicked(View view, int type) {
                     Log.e(TAG, "onAdClicked,type="+type);
-                    showToast("开屏广告点击");
                     int adType = type == 4 ? 1 : 2;
-                    defaultCallback.onAdClick(ClickModel.getInstance(0,adType,adId,Adv_Type.tt).setAdRespItem(getAdParamItem()));
+                    tt.defaultCallback.onAdClick(ClickModel.getInstance(0,adType,adId,Adv_Type.tt).setAdRespItem(adRespItem));
                 }
 
                 @Override
                 public void onAdShow(View view, int type) {
                     Log.e(TAG, "onAdShow,type="+type);
-                    showToast("开屏广告展示");
-                    if(call_back_count.get() == 1) {
-                        call_back_count.set(0);
-                        defaultCallback.onADExposed(PresentModel.getInstance(adId, Adv_Type.tt).setAdRespItem(getAdParamItem()));
+                    if(tt.call_back_count.get() == 1) {
+                        tt.call_back_count.set(0);
+                        tt.defaultCallback.onADExposed(PresentModel.getInstance(adId, Adv_Type.tt).setAdRespItem(adRespItem));
                     }
                 }
 
                 @Override
                 public void onAdSkip() {
                     Log.e(TAG, "onAdSkip");
-                    showToast("开屏广告跳过");
-                    defaultCallback.onAdSkip(PresentModel.getInstance(adId, Adv_Type.tt).setAdRespItem(getAdParamItem()));
+                    tt.defaultCallback.onAdSkip(PresentModel.getInstance(adId, Adv_Type.tt).setAdRespItem(adRespItem));
                 }
 
                 @Override
                 public void onAdTimeOver() {
                     Log.e(TAG, "onAdTimeOver");
-                    showToast("开屏广告倒计时结束");
-                    defaultCallback.onAdDismissed(DismissModel.newInstance(adId,Adv_Type.tt,2).setAdRespItem(getAdParamItem()));
+                    tt.defaultCallback.onAdDismissed(DismissModel.newInstance(adId,Adv_Type.tt,2).setAdRespItem(adRespItem));
                 }
             });
         }
@@ -198,15 +215,20 @@ public class SplashForTT extends ADBaseImpl {
             Log.e(TAG, "show(),mSplashContainer is null");
             return;
         }
+        ViewGroup vg = mSplashContainer.get();
+        if(vg == null){
+            Log.e(TAG, "show(),vg is null");
+            return;
+        }
         Log.e(TAG, "show()");
         //获取SplashView
         try {
             View view = respAd.getSplashView();
-            if(mSplashContainer.getChildCount() > 0) {
-                mSplashContainer.removeAllViews();
+            if(vg.getChildCount() > 0) {
+                vg.removeAllViews();
             }
             //把SplashView 添加到ViewGroup中,注意开屏广告view：width >=70%屏幕宽；height >=50%屏幕宽
-            mSplashContainer.addView(view);
+            vg.addView(view);
         } catch (Exception e) {
             Log.e(TAG, "show(),errMsg="+e.getMessage());
             e.printStackTrace();
